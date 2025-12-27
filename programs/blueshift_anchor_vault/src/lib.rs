1011010a5bac600a0e0029217c7162d002b615aa
abdullah <abdullah.se4th@gmail.com>
2025-12-27 10:21:19 +0500

    Add: instruction, accounts, error_code

--- programs/blueshift_anchor_vault/src/lib.rs
+++ programs/blueshift_anchor_vault/src/lib.rs
@@ -1,16 +1,39 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5hf3H8py8w5VjTrCeDMZw7yr6PEkiwhCB67wHdZ2cfp");
+// declare_id!("5hf3H8py8w5VjTrCeDMZw7yr6PEkiwhCB67wHdZ2cfp");
+declare_id!("22222222222222222222222222222222222222222222");
 
 #[program]
 pub mod blueshift_anchor_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct VaultAction<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(mut,
+    seeds = [b"vault_account", signer.key().as_ref()],
+    bump,
+    )]
+    pub vault_account: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum VaultError {
+    #[msg("Vault already exists.")]
+    VaultAlreadyExists,
+
+    #[msg("Invalid amount.")]
+    InvalidAmount,
+}
